7570299f5891e83de341e2b325b64f25ea5ba0dd
robsonsamp <robsonsamp@gmail.com>
2016-12-10 11:04:00 -0200

    Atualizacao de NameSpace

--- src/Permissoes/Perfis/IlluminatePerfilRepository.php
+++ src/Permissoes/Perfis/IlluminatePerfilRepository.php
@@ -10,7 +10,7 @@
  * This source file is subject to the 3-clause BSD License that is
  * bundled with this package in the LICENSE file.
  *
- * @package    Contfuncionalidade de Acesso
+ * @package    Contperfil de Acesso
  * @version    0.0.1
  * @author     Robson Sampaio
  * @license    BSD License (3-clause)
@@ -18,7 +18,7 @@
  * @link       http://xfusionsolution.com.br
  */
 
-namespace Xfusionsolution\Controledeacesso\Funcionalidades;
+namespace Xfusionsolution\Controledeacesso\Permissoes\Perfis;
 
 use Xfusionsolution\Controledeacesso\Permissoes\Perfis\PerfilRepositoryInterface;
 use Xfusionsolution\Suporte\Traits\RepositoryTrait;
